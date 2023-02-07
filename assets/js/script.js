d62e5260b681f2de59d2c6556e19ce651caaaf67
nav2010 <39226429+nav2010@users.noreply.github.com>
2023-02-07 22:51:51 +0000

    add make variables

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let fiveDayEl = $(".fiveDay");
+let city = $("#search-input").val();
+let tempEl = $("#temperature");
+let cityEl = $("#city");
+let dateEl = $("#date");
+let iconEl = $("#weather-icon");
+let apikey =$("");
